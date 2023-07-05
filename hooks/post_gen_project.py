937b5c54c4176a8baaaab8ea8290d1fd2320828d
Bart Doekemeijer <bart.doekemeijer@shell.com>
2023-07-05 10:14:06 +0200

    Add options to import various types of examples from flasc

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -24,41 +24,41 @@ from zipfile import ZipFile
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
 
-def convert_ipynb_to_py(filepath):
-    if not ".ipynb" in filepath:
-        raise UserWarning("Invalid file type.")
+# def convert_ipynb_to_py(filepath):
+#     if not ".ipynb" in filepath:
+#         raise UserWarning("Invalid file type.")
 
-    indentation = ""
-    code = json.load(open(filepath))
-    py_file = open(filepath.replace(".ipynb", ".py"), "w+")
+#     indentation = ""
+#     code = json.load(open(filepath))
+#     py_file = open(filepath.replace(".ipynb", ".py"), "w+")
 
-    # Find line number where we finish imports
-    first_20_lines = np.hstack([c["source"] for c in code['cells']])[0:20]
-    ln_imports = np.where([(c[0:6] == "import") or (c[0:4] == "from") for c in first_20_lines])[0][-1] + 1
+#     # Find line number where we finish imports
+#     first_20_lines = np.hstack([c["source"] for c in code['cells']])[0:20]
+#     ln_imports = np.where([(c[0:6] == "import") or (c[0:4] == "from") for c in first_20_lines])[0][-1] + 1
 
-    # Cycle through lines
-    ln = 0
-    for cell in code['cells']:
-        if cell['cell_type'] == 'code':
-            #py_file.write('# -------- code --------\n')
-            for line in cell['source']:
-                # Apply manipulations, if necessary
-                if (ln == ln_imports):
-                    py_file.write("\n")
-                    py_file.write('if __name__ == "__main__":\n')
-                    indentation = "    "
-                else:
-                    line = line.replace("os.getcwd()", "os.path.dirname(os.path.abspath(__file__))")
+#     # Cycle through lines
+#     ln = 0
+#     for cell in code['cells']:
+#         if cell['cell_type'] == 'code':
+#             #py_file.write('# -------- code --------\n')
+#             for line in cell['source']:
+#                 # Apply manipulations, if necessary
+#                 if (ln == ln_imports):
+#                     py_file.write("\n")
+#                     py_file.write('if __name__ == "__main__":\n')
+#                     indentation = "    "
+#                 else:
+#                     line = line.replace("os.getcwd()", "os.path.dirname(os.path.abspath(__file__))")
 
-                py_file.write("{:s}{:s}".format(indentation, line))
-                ln += 1
-            py_file.write("\n\n")
-        elif cell['cell_type'] == 'markdown':
-            for line in cell['source']:
-                py_file.write("{:s}{:s} {:s}".format(indentation, "#", line))
-            py_file.write("\n")
-            ln += 1
-    py_file.close()
+#                 py_file.write("{:s}{:s}".format(indentation, line))
+#                 ln += 1
+#             py_file.write("\n\n")
+#         elif cell['cell_type'] == 'markdown':
+#             for line in cell['source']:
+#                 py_file.write("{:s}{:s} {:s}".format(indentation, "#", line))
+#             py_file.write("\n")
+#             ln += 1
+#     py_file.close()
 
 
 def remove_file(filepath):
@@ -82,7 +82,7 @@ def replace_str_in_python_file(filepath, str_old, str_new):
     os.rename(tmp_file, filepath)
 
 
-def download_flasc_examples(branch: str = "main", chunk_size=120):
+def download_flasc_examples(branch: str = "main", subfolder: str = "examples_artificial_data", chunk_size=120):
     if branch == "main":
         url = r"https://github.com/NREL/flasc/archive/refs/heads/main.zip"
     elif branch == "develop":
@@ -109,35 +109,42 @@ def download_flasc_examples(branch: str = "main", chunk_size=120):
     
     # Move all example files over
     print("Importing FLASC example scripts and customizing to this repository...")
-    src_path = os.path.join(tmp_path, f"flasc-{branch}", "examples_artificial_data")
+    src_path = os.path.join(tmp_path, f"flasc-{branch}", subfolder)
 
     # Now replace certain strings in all files
     py_files = [
         *glob.glob(os.path.join(src_path, "**", "*.py")),
         *glob.glob(os.path.join(src_path, "**", "*.ipynb"))
     ]
-    for fn in py_files:
-        replace_str_in_python_file(fn, "from flasc.examples.models import load_floris_artificial as load_floris", "from {{cookiecutter.project_slug}}.models import load_floris")
 
-    # Now move raw data file to 'common_windfarm_information' folder
-    shutil.move(
-        os.path.join(src_path, "raw_data_processing", "data", "raw_artificial_data.zip"),
-        os.path.join(root_path, "..", "{{cookiecutter.project_slug}}", "common_windfarm_information", "raw_artificial_data.zip"),
-        copy_function=shutil.copy2
-    )
+    if subfolder == "examples_artificial_data":
+        # Format artificial data processing files into FCT format
+        for fn in py_files:
+            replace_str_in_python_file(fn, "from flasc.examples.models import load_floris_artificial as load_floris", "from {{cookiecutter.project_slug}}.models import load_floris")
 
-    # Replace where we load the raw data file from, since now the file has been moved
-    replace_str_in_python_file(
-        os.path.join(src_path, "raw_data_processing", "filter_ws_power_curves.ipynb"),
-        'source_path = os.path.join(root_path, \\"data\\")',
-        'source_path = os.path.join(root_path, \\"..\\", \\"..\\", \\"common_windfarm_information\\")'
-    )
+        # Now move raw data file to 'common_windfarm_information' folder
+        shutil.move(
+            os.path.join(src_path, "raw_data_processing", "data", "raw_artificial_data.zip"),
+            os.path.join(root_path, "..", "{{cookiecutter.project_slug}}", "common_windfarm_information", "raw_artificial_data.zip"),
+            copy_function=shutil.copy2
+        )
 
-    replace_str_in_python_file(
-        os.path.join(src_path, "raw_data_processing", "filter_ws_power_curves.ipynb"),
-        'zipfile.extractall(\\"data\\")',
-        'zipfile.extractall(source_path)'
-    )
+        # Replace where we load the raw data file from, since now the file has been moved
+        replace_str_in_python_file(
+            os.path.join(src_path, "raw_data_processing", "filter_ws_power_curves.ipynb"),
+            'source_path = os.path.join(root_path, \\"data\\")',
+            'source_path = os.path.join(root_path, \\"..\\", \\"..\\", \\"common_windfarm_information\\")'
+        )
+
+        replace_str_in_python_file(
+            os.path.join(src_path, "raw_data_processing", "filter_ws_power_curves.ipynb"),
+            'zipfile.extractall(\\"data\\")',
+            'zipfile.extractall(source_path)'
+        )
+    else:
+        # Format Smarteole data processing files into FCT format
+        for fn in py_files:
+            replace_str_in_python_file(fn, "from flasc.examples.models import load_floris_smarteole as load_floris", "from {{cookiecutter.project_slug}}.models import load_floris")
 
     # Now move remaining Python files to correct directory
     for subpath in glob.glob(os.path.join(src_path, "*")):
@@ -147,12 +154,26 @@ def download_flasc_examples(branch: str = "main", chunk_size=120):
             copy_function=shutil.copy2
         )
 
+    # Now copy models.py and input files from flasc/examples/models.py
+    shutil.move(
+        os.path.join(src_path, "..", "flasc", "examples", "models.py"),
+        os.path.join(root_path, "..", "{{cookiecutter.project_slug}}", "python", "{{cookiecutter.project_slug}}", "models.py"),
+        copy_function=shutil.copy2
+    )
+    
     # Remove 'tmp' directory
     shutil.rmtree(tmp_path)
 
 if __name__ == '__main__':
-    # Download file
-    download_flasc_examples(branch='develop')
+    # Populate repository with examples, if necessary
+    if '{{ cookiecutter.populate_with_examples }}' == "Artificial SCADA data analysis examples from the 'flasc/main' branch":
+        download_flasc_examples(branch='main')
+    elif '{{ cookiecutter.populate_with_examples }}' == "Artificial SCADA data analysis examples from the 'flasc/develop' branch":
+        download_flasc_examples(branch='develop')
+    elif '{{ cookiecutter.populate_with_examples }}' == "Smarteole wake steering campaign analysis examples from the 'flasc/main' branch":
+        download_flasc_examples(branch='main')
+    elif '{{ cookiecutter.populate_with_examples }}' == "Smarteole wake steering campaign analysis examples from the 'flasc/develop' branch":
+        download_flasc_examples(branch='develop')
 
     # # Remove example directories
     # if '{{ cookiecutter.populate_with_examples }}' != 'y':
